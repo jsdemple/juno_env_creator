b8ca532cee28453b6fc587fd0d4066de8a21a452
Joey Demple <jsdemple@gmail.com>
2017-05-27 16:38:37 -0600

    Create juno_env_creator.sh

---
+++ juno_env_creator.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+# create new conda environment
+# name is given as first argument
+conda create -n $1
+
+# activate the new environment and install ipykernel
+source activate $1
+pip install ipykernel
+
+# make your new environment available as an ipykernel in jupyter
+python -m ipykernel install --user --name $1 --display-name "$1"
