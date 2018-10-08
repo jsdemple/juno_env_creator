61501a0a788e1d4a7c46ccb9f6f5610089f95826
Joey Demple <jsdemple@gmail.com>
2018-10-08 11:59:37 -0600

    add pip install --upgrade pip

--- juno_env_creator.sh
+++ juno_env_creator.sh
@@ -7,6 +7,7 @@ conda create -n $1
 # activate the new environment and install ipykernel
 source activate $1
 conda install pip
+pip install --upgrade pip
 pip install ipykernel
 
 # make your new environment available as an ipykernel in jupyter
